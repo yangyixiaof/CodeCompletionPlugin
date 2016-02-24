61b263e51469c613910d018bfb5470ad6562a574
yyx <yangyixiaofirst@163.com>
2016-02-25 02:18:27 +0800

    small change.

--- cn.yyx.research.codecompleteplugin/src/cn/yyx/research/language/JDTHelper/SystemParameter.java
+++
@@ -1,7 +0,0 @@
-package cn.yyx.research.language.JDTHelper;
-
-public class SystemParameter {
-	
-	public static final int MaxTry = 5;
-	
-}
--- cn.yyx.research.codecompleteplugin/src/cn/yyx/research/language/JDTManager/NodeTypeLibrary.java
+++
@@ -1,9 +0,0 @@
-package cn.yyx.research.language.JDTManager;
-
-public class NodeTypeLibrary {
-	
-	public static final Character adjacent = 'a';
-	public static final Character newstart = 'b';
-	public static final Character comphole = 'c';
-	
-}
