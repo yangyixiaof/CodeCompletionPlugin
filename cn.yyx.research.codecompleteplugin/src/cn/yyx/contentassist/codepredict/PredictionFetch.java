aff91b60e223071c2e4ce9721138fed452e55dd4
Yixiao Yang <yangyixiaofirst@163.com>
2016-02-28 22:34:41 +0800

    small change.

--- cn.yyx.research.codecompleteplugin/src/cn/yyx/contentassist/codepredict/PredictionFetch.java
+++ cn.yyx.research.codecompleteplugin/src/cn/yyx/contentassist/codepredict/PredictionFetch.java
@@ -1,39 +1,40 @@
 package cn.yyx.contentassist.codepredict;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 import cn.yyx.contentassist.commonutils.ListHelper;
-
+import cn.yyx.research.AeroSpikeHandle.AeroHelper;
 import cn.yyx.research.AeroSpikeHandle.AeroLifeCycle;
 
 public class PredictionFetch {
 	
-	public static int PrePredictWindow = 10;
+	public static final int PrePredictWindow = 10;
+	public static final double SequenceSimilarThreshold = 0.7;
+	public static final double OneSentenceSimilarThreshold = 0.7;
 	
 	public static void FetchPrediction(List<String> analist, ArrayList<String> result) {
+		
+		AeroLifeCycle.Initialize();
+		
 		int size = analist.size();
 		if (size > PrePredictWindow) {
 			analist = analist.subList(size - PrePredictWindow, size);
 			size = PrePredictWindow;
 		}
-		System.out.println("ArrayListType:" + analist.getClass());
-		System.out.println("ArrayListRealSize:" + analist.size() + ";OSize;" + size + ";They should be the same.");
-	}
-	
-	public static void FetchPrediction(ArrayList<String> analist, ArrayList<String> result)
-	{
-		AeroLifeCycle.Initialize();
 		
-		try {
-			// AeroHelper.testListStrings(1);
-			
-			
-			
-		} catch (Exception e) {
-			
-			e.printStackTrace();
+		Iterator<String> itr = analist.iterator();
+		while (itr.hasNext())
+		{
+			String ons = itr.next();
+			// TODO
 		}
+		
+		// AeroHelper.testListStrings(1);
+		
+		// System.out.println("ArrayListType:" + analist.getClass());
+		// System.out.println("ArrayListRealSize:" + analist.size() + ";OSize;" + size + ";They should be the same.");
 		
 		AeroLifeCycle.Destroy();
 	}
