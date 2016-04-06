adb2d348d6a6792146d299dcd7003fc79e97d561
yyx <yangyixiaofirst@163.com>
2016-04-06 11:55:37 +0800

    small change.

--- cn.yyx.research.codecompleteplugin/src/cn/yyx/contentassist/codesynthesis/CSFlowLineBackTraceGenerationHelper.java
+++ cn.yyx.research.codecompleteplugin/src/cn/yyx/contentassist/codesynthesis/CSFlowLineBackTraceGenerationHelper.java
@@ -1,8 +1,11 @@
 package cn.yyx.contentassist.codesynthesis;
 
+import cn.yyx.contentassist.codesynthesis.flowline.CSFlowLineData;
+import cn.yyx.contentassist.codesynthesis.flowline.FlowLineNode;
+
 public class CSFlowLineBackTraceGenerationHelper {
 	
-	public static void GenerateSynthesisCode(CSFlowLineQueue squeue)
+	public static void GenerateSynthesisCode(CSFlowLineQueue squeue, FlowLineNode<CSFlowLineData> stopnode)
 	{
 		
 	}
