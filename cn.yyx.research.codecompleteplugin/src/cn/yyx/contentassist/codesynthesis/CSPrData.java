37bbc7f5fd46f82f8e61678948f8a9a6afcc42b5
yyx <yangyixiaofirst@163.com>
2016-04-06 23:00:34 +0800

    small change.

---
+++ cn.yyx.research.codecompleteplugin/src/cn/yyx/contentassist/codesynthesis/CSPrData.java
@@ -0,0 +1,16 @@
+package cn.yyx.contentassist.codesynthesis;
+
+import cn.yyx.contentassist.codepredict.Sentence;
+import cn.yyx.contentassist.codesynthesis.flowline.CSFlowLineData;
+import cn.yyx.contentassist.codesynthesis.typeutil.TypeComputationKind;
+import cn.yyx.contentassist.commonutils.SynthesisHandler;
+
+public class CSPrData extends CSFlowLineData{
+	
+	public CSPrData(Integer id, Sentence sete, String data, Integer structsignal, Class<?> dcls, boolean hashole,
+			TypeComputationKind tck, SynthesisHandler handler) {
+		super(id, sete, data, structsignal, dcls, hashole, tck, handler);
+		this.setShouldskip(true);
+	}
+	
+}
