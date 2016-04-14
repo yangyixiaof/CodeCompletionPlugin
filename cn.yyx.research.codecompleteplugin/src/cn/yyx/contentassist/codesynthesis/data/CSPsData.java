e331019fbd208c14b80a178e66dade64aa8d1624
yyx <yangyixiaofirst@163.com>
2016-04-14 12:38:49 +0800

    small change.

--- cn.yyx.research.codecompleteplugin/src/cn/yyx/contentassist/codesynthesis/data/CSPsData.java
+++ cn.yyx.research.codecompleteplugin/src/cn/yyx/contentassist/codesynthesis/data/CSPsData.java
@@ -5,9 +5,9 @@ import cn.yyx.contentassist.codesynthesis.typeutil.TypeComputationKind;
 import cn.yyx.contentassist.commonutils.SynthesisHandler;
 
 public class CSPsData extends CSFlowLineData{
-
+	
 	public CSPsData(Integer id, Sentence sete, String data, Class<?> dcls, boolean haspre, boolean hashole, TypeComputationKind pretck, TypeComputationKind posttck, SynthesisHandler handler) {
 		super(id, sete, data, dcls, haspre, hashole, pretck, posttck, handler);
 	}
-
+	
 }
