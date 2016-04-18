3e1273f547cf725328a28df2a1f67a6c91f150ae
yyx <yangyixiaofirst@163.com>
2016-04-18 16:29:15 +0800

    small change.

--- cn.yyx.research.codecompleteplugin/src/cn/yyx/contentassist/intellijcodecompletion/TODO.java
+++ cn.yyx.research.codecompleteplugin/src/cn/yyx/contentassist/intellijcodecompletion/TODO.java
@@ -3,15 +3,16 @@ package cn.yyx.contentassist.intellijcodecompletion;
 public class TODO {
 	
 	// TODO exact match must merge the possibility.
-	// TODO other also needs to merge.
+	// TODO Some small part of a statement duplicate its possibility, this must be solved.
+	
+	// TODO multiple '(' or ')' should merge. multiple '{' or '}' should merge.
 	
 	// TODO do-while should before judge.
-	// TODO multiple ')' should merge. multiple '{' or '}' should merge.
-	
-	// TODO the sequence has not been eliminated by LCS.
-	
 	// TODO do statement should be eliminated. do-while become do.
-	// do-while use '{' to stop instead of ';'.
 	
-	// TODO String new this super.a system.out such must be handled. Include fieldAccess identifier.
+	// Solved. the sequence has not been eliminated by LCS.
+	
+	// do-while use '{'/itself to stop instead of ';'.
+	
+	// Solved. String new this super.a system.out such must be handled. Include fieldAccess identifier.
 }
