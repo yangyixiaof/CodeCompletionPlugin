70dbdb14222b0d91f8d7e255f230ef3499bbcb55
yyx <yyx@ubuntu>
2016-04-23 04:05:32 -0700

    small change.

--- cn.yyx.research.codecompleteplugin/src/cn/yyx/contentassist/parsehelper/ComplexParser.java
+++ cn.yyx.research.codecompleteplugin/src/cn/yyx/contentassist/parsehelper/ComplexParser.java
@@ -13,6 +13,10 @@ public class ComplexParser {
 			System.err.println("Parse One Sentence error! serious error, the system will exit. The error parsed setence is :" + str + ".");
 			e.printStackTrace();
 			System.exit(1);
+		} catch (Error e) {
+			System.err.println("Parse One Sentence error! serious error, the system will exit. The error parsed setence is :" + str + ".");
+			e.printStackTrace();
+			System.exit(1);
 		}
 		Sentence set = new Sentence(str, evalVisitor.getSmt());
 		return set;
