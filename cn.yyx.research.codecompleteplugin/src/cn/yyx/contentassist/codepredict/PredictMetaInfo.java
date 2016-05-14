633e91b9019f74e3fc049286cd8381f86aa06076
yyx <yangyixiaofirst@163.com>
2016-05-14 23:46:38 +0800

    small change.

--- cn.yyx.research.codecompleteplugin/src/cn/yyx/contentassist/codepredict/PredictMetaInfo.java
+++ cn.yyx.research.codecompleteplugin/src/cn/yyx/contentassist/codepredict/PredictMetaInfo.java
@@ -5,9 +5,9 @@ public class PredictMetaInfo {
 	public static final double NotExistProbability = 0;
 	
 	public static final double SequenceSimilarThreshold = 0.5;
-	public static final double OneSentenceSimilarThreshold = 0.7;
+	public static final double OneSentenceSimilarThreshold = 0.6;
 	
-	public static final double TwoStringSimilarThreshold = 0.65;
+	public static final double TwoStringSimilarThreshold = 0.6;
 	
 	public static final int PredictMaxSequence = 15;
 	public static final int PrePredictWindow = 8;
