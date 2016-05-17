db566cb64cbf09284cef868cc71fca805e992f34
yyx <yangyixiaofirst@163.com>
2016-05-17 12:22:21 +0800

    small change.

--- cn.yyx.research.codecompleteplugin/src/cn/yyx/contentassist/codepredict/PredictMetaInfo.java
+++ cn.yyx.research.codecompleteplugin/src/cn/yyx/contentassist/codepredict/PredictMetaInfo.java
@@ -11,10 +11,10 @@ public class PredictMetaInfo {
 	
 	public static final int PredictMaxSequence = 15;
 	public static final int PrePredictWindow = 8;
-	public static final int PreTryMaxStep = 6;
-	public static final int PreTryNeedSize = 6;
+	public static final int PreTryMaxStep = 8;
+	public static final int PreTryNeedSize = 4;
 	
-	public static final int ExtendFinalMaxSequence = 3;
+	public static final int ExtendFinalMaxSequence = 2;
 	public static final int ExtendTempMaxSequence = 8;
 	
 	public static final int MaxExtendLength = 15;
