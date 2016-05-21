534872d2cbdfda29b99cc532836fef0e740a93ec
yyx <yyx@ubuntu>
2016-05-21 08:26:17 -0700

    small change.

--- cn.yyx.research.codecompleteplugin/src/cn/yyx/contentassist/parsehelper/ComplexParser.java
+++ cn.yyx.research.codecompleteplugin/src/cn/yyx/contentassist/parsehelper/ComplexParser.java
@@ -5,6 +5,7 @@ import org.antlr.v4.runtime.RecognitionException;
 import org.antlr.v4.runtime.misc.ParseCancellationException;
 
 import cn.yyx.contentassist.codepredict.Sentence;
+import cn.yyx.contentassist.codeutils.type;
 import cn.yyx.parse.specialparse.ParseRoot;
 
 public class ComplexParser {
@@ -36,6 +37,34 @@ public class ComplexParser {
 		}
 		Sentence set = new Sentence(str, evalVisitor.getSmt());
 		return set;
+	}
+	
+	public static type GetType(String str) {
+		OneTypeVisitor evalVisitor = new OneTypeVisitor();
+		try {
+			ParseRoot.ParseOneType(str, evalVisitor, false);
+		} catch (Exception e) {
+			System.err.println("Parse One Sentence error! serious error, the system will exit. The error parsed setence is :" + str + ".");
+			if (e instanceof ParseCancellationException)
+			{
+				ParseCancellationException pce = (ParseCancellationException)e;
+				Throwable pc = pce.getCause();
+				if (pc instanceof RecognitionException)
+				{
+					RecognitionException re = (RecognitionException)pce.getCause();
+				    ParserRuleContext context = (ParserRuleContext)re.getCtx();
+				    System.err.println(re);
+				    System.err.println(context);
+				}
+			}
+			e.printStackTrace();
+			System.exit(1);
+		} catch (Error e) {
+			System.err.println("Parse One Sentence error! serious error, the system will exit. The error parsed setence is :" + str + ".");
+			e.printStackTrace();
+			System.exit(1);
+		}
+		return evalVisitor.getTp();
 	}
 	
 	public static Sentence GetSentenceWithNoExit(String str) {
