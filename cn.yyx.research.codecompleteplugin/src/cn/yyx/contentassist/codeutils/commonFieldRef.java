0a128122fd3494a324aaeeb54e490cd63d51f5f7
yyx <yangyixiaofirst@163.com>
2016-05-22 19:03:09 +0800

    small change.

--- cn.yyx.research.codecompleteplugin/src/cn/yyx/contentassist/codeutils/commonFieldRef.java
+++ cn.yyx.research.codecompleteplugin/src/cn/yyx/contentassist/codeutils/commonFieldRef.java
@@ -5,12 +5,12 @@ import java.util.Map;
 
 import cn.yyx.contentassist.codepredict.CodeSynthesisException;
 import cn.yyx.contentassist.codesynthesis.CSFlowLineQueue;
+import cn.yyx.contentassist.codesynthesis.CSVarRefHelper;
 import cn.yyx.contentassist.codesynthesis.CodeSynthesisHelper;
 import cn.yyx.contentassist.codesynthesis.data.CSFlowLineData;
 import cn.yyx.contentassist.codesynthesis.flowline.FlowLineNode;
 import cn.yyx.contentassist.codesynthesis.statementhandler.CSStatementHandler;
 import cn.yyx.contentassist.commonutils.SimilarityHelper;
-import cn.yyx.contentassist.commonutils.VariableHT;
 
 public class commonFieldRef extends fieldAccess{
 	
@@ -43,22 +43,15 @@ public class commonFieldRef extends fieldAccess{
 	@Override
 	public List<FlowLineNode<CSFlowLineData>> HandleCodeSynthesis(CSFlowLineQueue squeue, CSStatementHandler smthandler)
 			throws CodeSynthesisException {
-		Map<String, String> po = squeue.GetLastHandler().getScopeOffsetRefHandler().HandleFieldVariableRef(scope, off);
-		if (scope == 0 && smthandler.getAoi().isInFieldLevel())
-		{
-			VariableHT vht = squeue.BackSearchForLastIthVariableHolderAndTypeDeclaration(off);
-			if (vht != null)
-			{
-				po.put(vht.getHoldertype(), vht.getHoldername());
-			}
-		}
+		Map<String, String> po = CSVarRefHelper.GetAllFieldTypeVariablePair(squeue, smthandler, scope, off);
 		return CodeSynthesisHelper.HandleVarRefCodeSynthesis(po, squeue, smthandler);
 	}
 	
 	@Override
 	public List<FlowLineNode<CSFlowLineData>> HandleInferredField(CSFlowLineQueue squeue, CSStatementHandler smthandler,
 			String reservedword, List<FlowLineNode<CSFlowLineData>> expectedinfer) throws CodeSynthesisException {
-		Map<String, String> po = squeue.GetLastHandler().getScopeOffsetRefHandler().HandleFieldVariableRef(scope, off);
+		Map<String, String> po = CSVarRefHelper.GetAllFieldTypeVariablePair(squeue, smthandler, scope, off);
+		// Map<String, String> po = squeue.GetLastHandler().getScopeOffsetRefHandler().HandleFieldVariableRef(scope, off);
 		return CodeSynthesisHelper.HandleVarRefInferredField(po, squeue, smthandler, reservedword, expectedinfer);
 	}
 
@@ -66,7 +59,8 @@ public class commonFieldRef extends fieldAccess{
 	public List<FlowLineNode<CSFlowLineData>> HandleInferredMethodReference(CSFlowLineQueue squeue,
 			CSStatementHandler smthandler, String reservedword, List<FlowLineNode<CSFlowLineData>> expectedinfer)
 			throws CodeSynthesisException {
-		Map<String, String> po = squeue.GetLastHandler().getScopeOffsetRefHandler().HandleFieldVariableRef(scope, off);
+		Map<String, String> po = CSVarRefHelper.GetAllFieldTypeVariablePair(squeue, smthandler, scope, off);
+		// Map<String, String> po = squeue.GetLastHandler().getScopeOffsetRefHandler().HandleFieldVariableRef(scope, off);
 		return CodeSynthesisHelper.HandleVarRefInferredMethodReference(po, squeue, smthandler, reservedword, expectedinfer);
 	}
 	
