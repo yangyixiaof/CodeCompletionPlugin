e44a4ce11eda112a5eec03bc0bb3cbec91c7aabb
yyx <yangyixiaofirst@163.com>
2016-05-26 18:19:54 +0800

    small change.

--- cn.yyx.research.codecompleteplugin/src/cn/yyx/contentassist/codecompletion/CodeCompletionMetaInfo.java
+++ cn.yyx.research.codecompleteplugin/src/cn/yyx/contentassist/codecompletion/CodeCompletionMetaInfo.java
@@ -2,7 +2,7 @@ package cn.yyx.contentassist.codecompletion;
 
 public class CodeCompletionMetaInfo {
 	
-	public static final String ServerIp = "192.168.1.103"; // 127.0.0.1
+	public static final String ServerIp = "192.168.1.101"; // 127.0.0.1
 	public static final boolean DebugMode = true;
 	
 }
