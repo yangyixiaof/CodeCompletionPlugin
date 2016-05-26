c04a81e78d01bfa893fa0513f39e542a2c903643
yyx <yangyixiaofirst@163.com>
2016-05-26 15:52:26 +0800

    small change.

--- cn.yyx.research.codecompleteplugin/src/cn/yyx/contentassist/codecompletion/PredictMetaInfo.java
+++ cn.yyx.research.codecompleteplugin/src/cn/yyx/contentassist/codecompletion/PredictMetaInfo.java
@@ -17,10 +17,10 @@ public class PredictMetaInfo {
 	public static final int PreTryMaxStep = 4;
 	public static final int PreTryNeedSize = 2;
 	
-	public static final int OneExtendFirstTotalStep = 50;
+	public static final int OneExtendFirstTotalStep = 20;
 	public static final int OneFirstMaxTotalSuccess = 1;
 	public static final int OneExtendFirstMaxSequence = 2;
-	public static final int OneExtendMaxSequence = 8;
+	public static final int OneExtendMaxSequence = 4;
 	public static final int OneLevelExtendMaxSequence = 1; // must be the power of 2.
 	
 	public static final int MaxExtendLength = 15;
