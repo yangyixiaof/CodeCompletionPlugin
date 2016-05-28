f32d36a67264590ed171ebadedf1de478cf3af7b
yyx <yangyixiaofirst@163.com>
2016-05-28 23:05:47 +0800

    small change.

---
+++ cn.yyx.research.codecompleteplugin/backupcode/TypeCheckHelper.java
@@ -0,0 +1,68 @@
+package cn.yyx.contentassist.codesynthesis.typeutil;
+
+import java.util.Iterator;
+import java.util.List;
+
+public class TypeCheckHelper {
+	
+	/*public static boolean CanBeMutualCast(TypeCheck one, TypeCheck two)
+	{
+		Class<?> onetype = one.getExpreturntypeclass();
+		Class<?> twotype = two.getExpreturntypeclass();
+		return CanBeMutualCast(onetype, twotype);
+	}*/
+	
+	public static boolean CanBeMutualCast(List<CCType> cs, CCType rtclass) {
+		Iterator<CCType> itr = cs.iterator();
+		while (itr.hasNext())
+		{
+			CCType cct = itr.next();
+			if (CanBeMutualCast(cct, rtclass))
+			{
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	public static boolean CanBeMutualCast(CCType onetype, CCType twotype)
+	{
+		if (onetype == null || twotype == null)
+		{
+			return true;
+		}
+		Class<?> ot = NormalizeClass(onetype.getCls());
+		Class<?> tt = NormalizeClass(twotype.getCls());
+		if (ot.isAssignableFrom(tt) || tt.isAssignableFrom(ot))
+		{
+			return true;
+		}
+		return false;
+	}
+	
+	public static Class<?> NormalizeClass(Class<?> onetype)
+	{
+		if (onetype.equals(int.class))
+		{
+			onetype = Integer.class;
+		}
+		if (onetype.equals(boolean.class))
+		{
+			onetype = Boolean.class;
+		}
+		if (onetype.equals(float.class))
+		{
+			onetype = Float.class;
+		}
+		if (onetype.equals(double.class))
+		{
+			onetype = Double.class;
+		}
+		if (onetype.equals(char.class))
+		{
+			onetype = Character.class;
+		}
+		return onetype;
+	}
+	
+}
