cec95bc0eb6c1235e17828b59d11dd64f3334dfc
yyx <yangyixiaofirst@163.com>
2016-06-06 12:06:53 +0800

    small chagne.

--- cn.yyx.research.codecompleteplugin/src/cn/yyx/contentassist/commonutils/ProposalHelper.java
+++ cn.yyx.research.codecompleteplugin/src/cn/yyx/contentassist/commonutils/ProposalHelper.java
@@ -1,13 +1,25 @@
 package cn.yyx.contentassist.commonutils;
 
+import java.io.InputStream;
 import java.util.Iterator;
 import java.util.List;
 
 import org.eclipse.jdt.ui.text.java.JavaContentAssistInvocationContext;
 import org.eclipse.jface.text.contentassist.CompletionProposal;
 import org.eclipse.jface.text.contentassist.ICompletionProposal;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.graphics.ImageData;
 
 public class ProposalHelper {
+	
+	private static Image image = null;
+	
+	static {
+		InputStream stream = ProposalHelper.class.getClassLoader().getResourceAsStream("/cn/yyx/contentassist/commonutils/cc.png");
+		ImageData id = new ImageData(stream);
+		image = new Image(null, id);
+		// image = ImageIO.read(new File("icons/cc.png"));
+	}
 	
 	public static void ProposalContentToFormalFormat(JavaContentAssistInvocationContext context, List<String> proposalcnt, List<ICompletionProposal> proposals)
 	{
@@ -15,7 +27,9 @@ public class ProposalHelper {
 		while (itr.hasNext())
 		{
 			String pol = itr.next();
-			proposals.add(new CompletionProposal(pol, context.getInvocationOffset(), 0, pol.length()));
+			proposals.add(new CompletionProposal(pol, 0, pol.length(), 0, image, pol.replace('\n', ' '), null, null));
+			
+			// (pol, context.getInvocationOffset(), 0, pol.length())
 		}
 	}
 	
