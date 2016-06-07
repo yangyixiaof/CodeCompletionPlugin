05ec0494af8f848175b1bd1bc60bee081d563eaf
yyx <yangyixiaofirst@163.com>
2016-06-07 12:19:44 +0800

    small change.

--- cn.yyx.research.codecompleteplugin/src/cn/yyx/contentassist/codeutils/rawForUpdOverStatement.java
+++ cn.yyx.research.codecompleteplugin/src/cn/yyx/contentassist/codeutils/rawForUpdOverStatement.java
@@ -1,5 +1,6 @@
 package cn.yyx.contentassist.codeutils;
 
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Stack;
 
@@ -23,10 +24,18 @@ public class rawForUpdOverStatement extends statement {
 	@Override
 	public List<FlowLineNode<CSFlowLineData>> HandleCodeSynthesis(CSFlowLineQueue squeue, CSStatementHandler smthandler)
 			throws CodeSynthesisException {
+		List<FlowLineNode<CSFlowLineData>> result = new LinkedList<FlowLineNode<CSFlowLineData>>();
 		FlowLineNode<CSFlowLineData> fln = new FlowLineNode<CSFlowLineData>(new CSFlowLineData(squeue.GenerateNewNodeId() + "", smthandler.getSete(),
 				") {\n}", null, null, squeue.GetLastHandler(), new CSForUpdOverProperty(null)),
 				smthandler.getProb());
-		return CSFlowLineBackTraceGenerationHelper.GenerateNotYetAddedSynthesisCode(squeue, smthandler, fln, null);
+		List<FlowLineNode<CSFlowLineData>> rls = CSFlowLineBackTraceGenerationHelper.GenerateNotYetAddedSynthesisCode(squeue, smthandler, fln, null);
+		if (rls != null && rls.size() > 0)
+		{
+			fln.setSynthesisdata(rls.get(0).getData());
+		}
+		result.add(fln);
+		return result;
+		// return CSFlowLineBackTraceGenerationHelper.GenerateNotYetAddedSynthesisCode(squeue, smthandler, fln, null);
 	}
 
 	@Override
