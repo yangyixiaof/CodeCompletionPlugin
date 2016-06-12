affdc733d3ac7e13149d2aa1e4ea6a53a788afad
yyx <yangyixiaofirst@163.com>
2016-06-13 00:18:00 +0800

    small change.

--- cn.yyx.research.codecompleteplugin/src/cn/yyx/contentassist/codesynthesis/data/CSArrayAccessStartProperty.java
+++ cn.yyx.research.codecompleteplugin/src/cn/yyx/contentassist/codesynthesis/data/CSArrayAccessStartProperty.java
@@ -6,33 +6,29 @@ import cn.yyx.contentassist.codepredict.CodeSynthesisException;
 import cn.yyx.contentassist.commonutils.ComplicatedSignal;
 
 public class CSArrayAccessStartProperty extends CSExtraProperty {
-	
+
 	public CSArrayAccessStartProperty(CSExtraProperty csepnext) {
 		super(csepnext);
 	}
 
 	@Override
-	public void HandleStackSignalDetail(Stack<Integer> signals) throws CodeSynthesisException{
-		if (signals.size() == 0)
-		{
+	public void HandleStackSignalDetail(Stack<Integer> signals) throws CodeSynthesisException {
+		if (signals.size() == 0) {
 			throw new CodeSynthesisException("array access does not in right block.");
 		}
 		Integer sl = signals.peek();
-		if (sl != null)
-		{
-			ComplicatedSignal cs = ComplicatedSignal.ParseComplicatedSignal(sl);
-			if (cs.getSign() == DataStructureSignalMetaInfo.ArrayAccessBlcok)
-			{
-				int count = cs.getCount()-1;
-				signals.pop();
-				if (count > 0)
-				{
-					cs.setCount(count);
-					signals.push(cs.GetSignal());
-				}
+		ComplicatedSignal cs = ComplicatedSignal.ParseComplicatedSignal(sl);
+		if (cs.getSign() == DataStructureSignalMetaInfo.ArrayAccessBlcok) {
+			int count = cs.getCount() - 1;
+			signals.pop();
+			if (count > 0) {
+				cs.setCount(count);
+				signals.push(cs.GetSignal());
 			}
+			return;
+		} else {
+			throw new CodeSynthesisException("array access does not in right block.");
 		}
-		throw new CodeSynthesisException("array access does not in right block.");
 	}
-	
+
 }
