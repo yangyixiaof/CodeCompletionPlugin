2c14743fb341a20bdae3cc24ae8be066ca21015f
yyx <yangyixiaofirst@163.com>
2016-06-15 10:19:33 +0800

    small change.

---
+++ cn.yyx.research.codecompleteplugin/src/cn/yyx/contentassist/codesynthesis/typeutil/PrimitiveTypeConflict.java
@@ -0,0 +1,11 @@
+package cn.yyx.contentassist.codesynthesis.typeutil;
+
+public class PrimitiveTypeConflict extends TypeConflictException{
+	
+	public PrimitiveTypeConflict(String info) {
+		super(info);
+	}
+
+	private static final long serialVersionUID = 1L;
+
+}
